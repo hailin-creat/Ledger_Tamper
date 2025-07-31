4b300c0a9590ff6f34eaa6de4665f2eb11b6222b
hailin-creat <hailin3932@163.com>
2025-07-31 16:40:36 +0800

    Update demo.c

--- demo.c
+++ demo.c
@@ -2,23 +2,23 @@
 #include <string.h>
 
 int check_password(const char *pwd) {
-    // 正确密码
+    // Correct password
     return strcmp(pwd, "123456") == 0;
 }
 
 void do_sensitive_action() {
-    printf("[SecureApp] 已执行敏感操作！\n");
+    printf("[SecureApp] Sensitive operation executed!\n");
 }
 
 int main() {
     char input[100];
-    printf("请输入密码: ");
+    printf("Please enter password: ");
     scanf("%99s", input);
 
     if (check_password(input)) {
         do_sensitive_action();
     } else {
-        printf("[SecureApp] 密码错误，拒绝执行。\n");
+        printf("[SecureApp] Incorrect password, access denied.\n");
     }
 
     return 0;
